6d7ef43b098fac8b1d417207e366d0d2f07e968f
s37 <s37@sagdomain.local>
2025-11-27 18:45:27 +0530

    updated code

--- frontend/src/app/(auth)/register/page.tsx
+++ frontend/src/app/(auth)/register/page.tsx
@@ -5,17 +5,22 @@ import { useRouter } from "next/navigation";
 import { useDispatch } from "react-redux";
 import { loginSuccess } from "@/store/slice/authSlice";
 import { LoginForm } from "@/components/login-form";
+import { RegisterForm } from "@/components/Register-form";
+import Image from "next/image";
 
 export default function RegisterPage() {
-  const [username, setUsername] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
-  const dispatch = useDispatch();
-  const router = useRouter();
-
 
   return (
-   <div><LoginForm/></div>
+      <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+         <div className="w-full max-w-sm md:max-w-lg">
+           <a href="#" className="flex items-center justify-center mb-5 gap-2 self-center font-medium">
+             <div className="  flex size-6 items-center justify-center rounded-md">
+               <Image src={require('@/assets/icon.png')} alt="icon"/>
+             </div>
+             Snapora
+           </a>
+         <RegisterForm/>
+         </div>
+       </div>
   );
 }
--- frontend/src/constant/ApiUrls.ts
+++ frontend/src/constant/ApiUrls.ts
@@ -3,3 +3,4 @@ export const BASE_URL = "http://192.168.0.34:4040/api/v1"
 // Auth urls 
 export const LOGIN_USER = "/auth/login"
 export const GET_USER_DATA = "/auth/get-user-data"
+export const REGISTER_USER = "/auth/register"
